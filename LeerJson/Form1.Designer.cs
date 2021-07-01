4f9c6042f5826e69b52403f38792d0f779ba554a
David-EV <davidenriquez72@hotmail.com>
2021-06-30 19:34:36 -0500

    solucion

---
+++ LeerJson/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LeerJson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridDatos = new System.Windows.Forms.DataGridView();
+            this.btnLeerArchivo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridDatos
+            // 
+            this.gridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDatos.Location = new System.Drawing.Point(24, 47);
+            this.gridDatos.Name = "gridDatos";
+            this.gridDatos.Size = new System.Drawing.Size(411, 182);
+            this.gridDatos.TabIndex = 0;
+            // 
+            // btnLeerArchivo
+            // 
+            this.btnLeerArchivo.Location = new System.Drawing.Point(24, 265);
+            this.btnLeerArchivo.Name = "btnLeerArchivo";
+            this.btnLeerArchivo.Size = new System.Drawing.Size(411, 46);
+            this.btnLeerArchivo.TabIndex = 1;
+            this.btnLeerArchivo.Text = "Mostrar datos";
+            this.btnLeerArchivo.UseVisualStyleBackColor = true;
+            this.btnLeerArchivo.Click += new System.EventHandler(this.btnLeerArchivo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ubicaciones";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 337);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLeerArchivo);
+            this.Controls.Add(this.gridDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridDatos;
+        private System.Windows.Forms.Button btnLeerArchivo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LeerJson/Form1.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeerJson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeerArchivo_Click(object sender, EventArgs e)
+        {
+            string fileJson = File.ReadAllText(@"D:\c#\archivo.json");
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(fileJson, typeof(DataTable));
+            gridDatos.DataSource = dt;
+        }
+    }
+}
